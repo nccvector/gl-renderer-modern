c97974f0bcd862f1722ff910d01ea82c9593ff8c
nccvector <fzn1020@gmail.com>
2024-05-02 22:53:57 +0900

    making a window

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main() {
+  glfwInit();
+  glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+  glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+  glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
+
+  GLFWwindow* window = glfwCreateWindow( 800, 600, "LearnOpenGL", NULL, NULL );
+  if ( window == NULL ) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent( window );
+
+  while ( !glfwWindowShouldClose( window ) ) {
+    glfwSwapBuffers( window );
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
